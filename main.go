78e66b5352b411608b58996fd77e6ef8338fe2d4
linkc0829 <linkc0829@gmail.com>
2020-12-23 05:17:54 +0000

    move main.go

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/linkc0829/go-ics/internal/mongodb"
+	"github.com/linkc0829/go-ics/pkg/server"
+	"github.com/linkc0829/go-ics/pkg/utils"
+)
+
+func main() {
+
+	var serverconf = &utils.ServerConfig{
+		Host:          utils.MustGet("SERVER_HOST"),
+		Port:          utils.MustGet("SERVER_PORT"),
+		URISchema:     utils.MustGet("SERVER_URI_SCHEMA"),
+		ApiVer:        utils.MustGet("SERVER_PATH_VERSION"),
+		SessionSecret: utils.MustGet("SESSION_SECRET"),
+		JWT: utils.JWTConfig{
+			Secret:             utils.MustGet("AUTH_JWT_SECRET"),
+			Algorithm:          utils.MustGet("AUTH_JWT_SIGNING_ALGORITHM"),
+			AccessTokenExpire:  utils.MustGet("AUTH_JWT_ACCESSTOKEN_EXPIRE"),
+			RefreshTokenExpire: utils.MustGet("AUTH_JWT_REFRESHTOKEN_EXPIRE"),
+		},
+		GraphQL: utils.GQLConfig{
+			Path:                utils.MustGet("GQL_SERVER_GRAPHQL_PATH"),
+			PlaygroundPath:      utils.MustGet("GQL_SERVER_GRAPHQL_PLAYGROUND_PATH"),
+			IsPlaygroundEnabled: utils.MustGetBool("GQL_SERVER_GRAPHQL_PLAYGROUND_ENABLED"),
+		},
+		MongoDB: utils.MGDBConfig{
+			DSN: utils.MustGet("MONGO_CONNECTION_DSN"),
+		},
+		AuthProviders: []utils.AuthProvider{
+			utils.AuthProvider{
+				Provider:  "google",
+				ClientKey: utils.MustGet("PROVIDER_GOOGLE_KEY"),
+				Secret:    utils.MustGet("PROVIDER_GOOGLE_SECRET"),
+			},
+			utils.AuthProvider{
+				Provider:  "auth0",
+				ClientKey: utils.MustGet("PROVIDER_AUTH0_KEY"),
+				Secret:    utils.MustGet("PROVIDER_AUTH0_SECRET"),
+				Domain:    utils.MustGet("PROVIDER_AUTH0_DOMAIN"),
+				Scopes:    strings.Split(utils.MustGet("PROVIDER_AUTH0_SCOPES"), ","),
+			},
+		},
+	}
+
+	db := mongodb.ConnectDB(serverconf)
+	defer mongodb.CloseDB(db)
+	server.Run(serverconf, db)
+}
+
