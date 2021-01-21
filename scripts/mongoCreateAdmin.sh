c3f4d4e5e0e6e7ac40be3115f6966c47f3c1f4e3
linkc0829 <linkc0829@gmail.com>
2021-01-21 15:49:06 +0800

    add mongo script

--- scripts/mongoCreateAdmin.sh
+++ scripts/mongoCreateAdmin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-mongo mongo:27017/users --eval "var user = '$MONGO_INITDB_ROOT_USERNAME', pwd = '$MONGO_INITDB_ROOT_PASSWORD'" scripts/createAdmin.js
+mongo --eval "var user = '$MONGO_INITDB_ROOT_USERNAME', pwd = '$MONGO_INITDB_ROOT_PASSWORD'" scripts/createAdmin.js
 echo "server admin created"
